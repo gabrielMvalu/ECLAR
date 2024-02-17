d1b8f3e0a1e34d84c015a975203b4df3a19e395a
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 20:46:11 +0200

    Update ECLAR.py

--- ECLAR.py
+++ ECLAR.py
@@ -28,39 +28,37 @@ def render_basic_pie_chart():
     st_pyecharts(c)
 
 def render_sales_line_chart():
-    # Lunile anului 2023
     months = ["Ian", "Feb", "Mar", "Apr", "Mai", "Iun", "Iul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    products = ["Mireasma din Tei", "Proaspăt ca Marea", "Parfumat ca Polenul", "Zero Parfum"]
     
-    # Culorile personalizate pentru fiecare produs
-    custom_colors = ["#f7cb39", "#4dc4be", "#ed6347", "#b0a989", "#34568B"]  # Am adăugat o culoare suplimentară pentru "Vânzări Totale"
+    # Culorile personalizate pentru fiecare produs și culoarea roșie pentru vânzările totale
+    custom_colors = ["#f7cb39", "#4dc4be", "#ed6347", "#b0a989", "RED"]
     
-    # Date fictive pentru vânzările lunare ale fiecărui produs
-    sales_data = {
-        "Mireasma din Tei": [120, 150, 180, 200, 220, 210, 200, 190, 180, 170, 160, 150],
-        "Proaspăt ca Marea": [80, 100, 120, 140, 160, 180, 200, 220, 200, 180, 160, 140],
-        "Parfumat ca Polenul": [50, 70, 90, 110, 130, 120, 110, 100, 90, 80, 70, 60],
-        "Zero Parfum": [200, 180, 160, 140, 120, 100, 80, 60, 40, 20, 10, 5]
-    }
+    # Date ajustate pentru vânzările lunare ale "Mireasma din Tei", începând din iunie
+    sales_mireasma = [0, 0, 0, 0, 0, 120, 150, 180, 210, 240, 270, 300]
+    sales_proaspat = [80, 100, 120, 140, 160, 180, 200, 220, 200, 180, 160, 140]
+    sales_parfumat = [50, 70, 90, 110, 130, 120, 110, 100, 90, 80, 70, 60]
+    sales_zero = [200, 180, 160, 140, 120, 100, 80, 60, 40, 20, 10, 5]
 
     # Calcularea vânzărilor totale pentru fiecare lună
-    sales_total = [sum(x) for x in zip(*sales_data.values())]
+    sales_total = [sum(x) for x in zip(sales_mireasma, sales_proaspat, sales_parfumat, sales_zero)]
 
     # Crearea graficului Line
     c = Line().add_xaxis(months)
 
-    # Adăugarea seriilor de date și a culorilor personalizate
-    for i, (product, sales) in enumerate(sales_data.items()):
-        c.add_yaxis(product, sales, is_smooth=True, 
-                    linestyle_opts=opts.LineStyleOpts(color=custom_colors[i]))
-
-    # Adăugarea seriei pentru vânzările totale
-    c.add_yaxis("Vânzări Totale", sales_total, is_smooth=True, 
-                linestyle_opts=opts.LineStyleOpts(color=custom_colors[-1], width=2, type_="dotted"))
+    # Adăugarea seriilor de date pentru fiecare produs și vânzările totale cu culorile specificate
+    c.add_yaxis(products[0], sales_mireasma, is_smooth=True, color=custom_colors[0])
+    c.add_yaxis(products[1], sales_proaspat, is_smooth=True, color=custom_colors[1])
+    c.add_yaxis(products[2], sales_parfumat, is_smooth=True, color=custom_colors[2])
+    c.add_yaxis(products[3], sales_zero, is_smooth=True, color=custom_colors[3])
+    c.add_yaxis("Vânzări Totale", sales_total, is_smooth=True, linestyle_opts=opts.LineStyleOpts(width=2, type_="dotted"), color=custom_colors[4])
 
     # Setarea opțiunilor globale
-    c.set_global_opts(title_opts=opts.TitleOpts(),
-                      yaxis_opts=opts.AxisOpts(name="Vânzări"),
-                      xaxis_opts=opts.AxisOpts(name="Lună"))
+    c.set_global_opts(
+        title_opts=opts.TitleOpts(),
+        yaxis_opts=opts.AxisOpts(name="Vânzări"),
+        xaxis_opts=opts.AxisOpts(name="Lună")
+    )
 
     st_pyecharts(c)
 
