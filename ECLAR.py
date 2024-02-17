3fca929f25fa59dab2b756164816219268aa8557
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 19:58:04 +0200

    Update ECLAR.py

--- ECLAR.py
+++ ECLAR.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit_timeline import timeline
 from pyecharts import options as opts
 from pyecharts.charts import Pie
-from pyecharts.faker import Faker
 from streamlit_echarts import st_pyecharts
 
 
@@ -21,13 +20,23 @@ with st.spinner(text="Building line"):
 
 
 
+
+
 def render_basic_pie_chart():
+    # Numele produselor
+    products = ["Mireasma din Tei", "Proaspăt ca Marea", "Parfumat ca Polenul", "Zero Parfum"]
+    
+    # Valorile vânzărilor pentru fiecare produs
+    # Acestea sunt doar exemple; înlocuiește-le cu valorile reale ale vânzărilor
+    sales_values = [100, 150, 200, 50]
+    
     c = (
         Pie()
-        .add("", [list(z) for z in zip(Faker.choose(), Faker.values())])
-        .set_global_opts(title_opts=opts.TitleOpts(title="Exemplu de diagramă Pie"))
+        .add("", [list(z) for z in zip(products, sales_values)])
+        .set_global_opts(title_opts=opts.TitleOpts(title="Vânzări Produse per Luna Curentă"))
         .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
     )
     st_pyecharts(c)
 
+# Apelarea funcției pentru a afișa graficul
 render_basic_pie_chart()
