1dbc4cf9509c2e187ec21b9c011fa291bc057184
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-18 00:04:20 +0200

    Update 4-Evenimente.py

--- pages/4-Evenimente.py
+++ pages/4-Evenimente.py
@@ -40,39 +40,34 @@ with elements("style_mui_sx"):
         }
     )
 
-with elements("dashboard"):
+with elements("monaco_editors"):
 
-    # You can create a draggable and resizable dashboard using
-    # any element available in Streamlit Elements.
+    # Streamlit Elements embeds Monaco code and diff editor that powers Visual Studio Code.
+    # You can configure editor's behavior and features with the 'options' parameter.
+    #
+    # Streamlit Elements uses an unofficial React implementation (GitHub links below for
+    # documentation).
 
-    from streamlit_elements import dashboard
+    from streamlit_elements import editor
 
-    # First, build a default layout for every element you want to include in your dashboard
+    if "content" not in st.session_state:
+        st.session_state.content = "Default value"
 
-    layout = [
-        # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
-        dashboard.Item("first_item", 0, 0, 2, 2),
-        dashboard.Item({st.session_state.my_text}, 2, 0, 2, 2, isDraggable=False, moved=False),
-        dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
-    ]
+    mui.Typography("Content: ", st.session_state.content)
 
-    # Next, create a dashboard layout using the 'with' syntax. It takes the layout
-    # as first parameter, plus additional properties you can find in the GitHub links below.
+    def update_content(value):
+        st.session_state.content = value
 
-    with dashboard.Grid(layout):
-        mui.Paper({st.session_state.my_text}, key="first_item")
-        mui.Paper("Second item (cannot drag)", key="second_item")
-        mui.Paper("Third item (cannot resize)", key="third_item")
+    editor.Monaco(
+        height=300,
+        defaultValue=st.session_state.content,
+        onChange=lazy(update_content)
+    )
 
-    # If you want to retrieve updated layout values as the user move or resize dashboard items,
-    # you can pass a callback to the onLayoutChange event parameter.
+    mui.Button("Update content", onClick=sync())
 
-    def handle_layout_change(updated_layout):
-        # You can save the layout in a file, or do anything you want with it.
-        # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
-        print(updated_layout)
-
-    with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
-        mui.Paper("First item", key="first_item")
-        mui.Paper("Second item (cannot drag)", key="second_item")
-        mui.Paper("Third item (cannot resize)", key="third_item")
+    editor.MonacoDiff(
+        original="Happy Streamlit-ing!",
+        modified="Happy Streamlit-in' with Elements!",
+        height=300,
+    )
