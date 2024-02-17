a1e33dea134759d8668e5fe0f268ad0a27661f1c
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 23:46:41 +0200

    Update 4-Evenimente.py

--- pages/4-Evenimente.py
+++ pages/4-Evenimente.py
@@ -1,9 +1,32 @@
 from streamlit_elements import elements, mui, html
 import streamlit as st
 
-with elements("multiple_children"):
+with elements("properties"):
 
-   with mui.Paper:
-        with mui.Typography:
-            html.p("Hello world")
-            html.p("Goodbye world")
+    # You can add properties to elements with named parameters.
+    #
+    # To find all available parameters for a given element, you can
+    # refer to its related documentation on mui.com for MUI widgets,
+    # on https://microsoft.github.io/monaco-editor/ for Monaco editor,
+    # and so on.
+    #
+    # <Paper elevation={3} variant="outlined" square>
+    #   <TextField label="My text input" defaultValue="Type here" variant="outlined" />
+    # </Paper>
+
+    with mui.Paper(elevation=3, variant="outlined", square=True):
+        mui.TextField(
+            label="My text input",
+            defaultValue="Type here",
+            variant="outlined",
+        )
+
+    # If you must pass a parameter which is also a Python keyword, you can append an
+    # underscore to avoid a syntax error.
+    #
+    # <Collapse in />
+
+    mui.Collapse(in_=True)
+
+    # mui.collapse(in=True)
+    # > Syntax error: 'in' is a Python keyword:
