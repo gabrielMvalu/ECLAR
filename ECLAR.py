483563e96599548c93b530d31907ab08789dcd31
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 20:25:39 +0200

    Update ECLAR.py

--- ECLAR.py
+++ ECLAR.py
@@ -31,22 +31,36 @@ def render_basic_pie_chart():
     )
     st_pyecharts(c)
 
-# Funcția pentru generarea diagramei Line
 def render_sales_line_chart():
-    products = ["Mireasma din Tei", "Proaspăt ca Marea", "Parfumat ca Polenul", "Zero Parfum"]
-    sales_values = [100, 150, 200, 50]
+    # Lunile anului 2023
+    months = ["Ian", "Feb", "Mar", "Apr", "Mai", "Iun", "Iul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     
+    # Date fictive pentru vânzările lunare ale fiecărui produs
+    sales_mireasma = [120, 150, 180, 200, 220, 210, 200, 190, 180, 170, 160, 150]
+    sales_proaspat = [80, 100, 120, 140, 160, 180, 200, 220, 200, 180, 160, 140]
+    sales_parfumat = [50, 70, 90, 110, 130, 120, 110, 100, 90, 80, 70, 60]
+    sales_zero = [200, 180, 160, 140, 120, 100, 80, 60, 40, 20, 10, 5]
+    
+    # Calcularea vânzărilor totale pentru fiecare lună
+    sales_total = [sum(x) for x in zip(sales_mireasma, sales_proaspat, sales_parfumat, sales_zero)]
+    
+    # Crearea graficului Line
     c = (
         Line()
-        .add_xaxis(products)
-        .add_yaxis("Vânzări", sales_values, is_smooth=True)
+        .add_xaxis(months)
+        .add_yaxis("Mireasma din Tei", sales_mireasma, is_smooth=True)
+        .add_yaxis("Proaspăt ca Marea", sales_proaspat, is_smooth=True)
+        .add_yaxis("Parfumat ca Polenul", sales_parfumat, is_smooth=True)
+        .add_yaxis("Zero Parfum", sales_zero, is_smooth=True)
+        .add_yaxis("Vânzări Totale", sales_total, is_smooth=True, linestyle_opts=opts.LineStyleOpts(width=2, type_="dotted"))
         .set_global_opts(
-            title_opts=opts.TitleOpts(title="Vânzări Produse per Luna Curentă"),
+            title_opts=opts.TitleOpts(title="Vânzări Produse pe Luna - 2023"),
             yaxis_opts=opts.AxisOpts(name="Vânzări"),
-            xaxis_opts=opts.AxisOpts(name="Produs")
+            xaxis_opts=opts.AxisOpts(name="Lună")
         )
     )
     st_pyecharts(c)
+
 
 # Crearea coloanelor și afișarea graficelor
 column_1, column_2 = st.columns(2)
