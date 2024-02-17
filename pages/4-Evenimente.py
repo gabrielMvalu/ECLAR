0360ec0dbf9c29a94ec081071a3984a47f92d46d
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 23:45:43 +0200

    Update 4-Evenimente.py

--- pages/4-Evenimente.py
+++ pages/4-Evenimente.py
@@ -3,33 +3,7 @@ import streamlit as st
 
 with elements("multiple_children"):
 
-    # You have access to Material UI icons using: mui.icon.IconNameHere
-    #
-    # Multiple children can be added in a single element.
-    #
-    # <Button>
-    #   <EmojiPeople />
-    #   <DoubleArrow />
-    #   Hello world
-    # </Button>
-
-    mui.Button(
-        mui.icon.EmojiPeople,
-        mui.icon.DoubleArrow,
-        "Button with multiple children"
-    )
-
-    # You can also add children to an element using a 'with' statement.
-    #
-    # <Button>
-    #   <EmojiPeople />
-    #   <DoubleArrow />
-    #   <Typography>
-    #     Hello world
-    #   </Typography>
-    # </Button>
-
-    with mui.Button:
-        mui.icon.EmojiPeople()
-        mui.icon.DoubleArrow()
-        mui.Typography("Button with multiple children")
+   with mui.Paper:
+        with mui.Typography:
+            html.p("Hello world")
+            html.p("Goodbye world")
