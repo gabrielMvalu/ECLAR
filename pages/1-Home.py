be5e18aaf2e21aa9c6e812ad0ffa3414dcae279f
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 22:37:40 +0200

    Update 1-Home.py

--- pages/1-Home.py
+++ pages/1-Home.py
@@ -1,10 +1,20 @@
-import streamlit as st 
+import streamlit as st
 from streamlit_timeline import timeline
 
 st.set_page_config(layout="wide")
-st.title(':blue[ECLAR SRL]')
-st.divider()
 
+# Utilizarea coloanelor pentru a plasa logo-ul lângă titlu
+col1, col2 = st.columns([1, 9])
+
+# În prima coloană, afișează logo-ul
+with col1:
+    st.image("./data/logoECLAR.png", width=100)  # Ajustează 'width' după necesități
+
+# În a doua coloană, afișează titlul
+with col2:
+    st.title(":blue:[ECLAR SRL]")
+
+st.divider()
 
 with st.spinner(text="Building line"):
     with open('timeline.json', "r") as f:
