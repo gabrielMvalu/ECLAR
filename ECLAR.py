f9dcb41fcbe7f417cffaa186bbf7a78cae04cccc
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 19:20:25 +0200

    Update ECLAR.py

--- ECLAR.py
+++ ECLAR.py
@@ -1,20 +1,12 @@
-import streamlit as st
+import streamlit as st 
 from streamlit_timeline import timeline
 
 st.set_page_config(layout="wide")
 st.header(':rainbow[Eclar STORY]')
 st.divider
 
-items = [
-    {"id": 1, "content": "2022-10-20", "start": "2022-10-20"},
-    {"id": 2, "content": "2022-10-09", "start": "2022-10-09"},
-    {"id": 3, "content": "2022-10-18", "start": "2022-10-18"},
-    {"id": 4, "content": "2022-10-16", "start": "2022-10-16"},
-    {"id": 5, "content": "2022-10-25", "start": "2022-10-25"},
-    {"id": 6, "content": "2022-10-27", "start": "2022-10-27"},
-]
 
-timeline = st.timeline(items, groups=[], options={}, height="300px")
-st.subheader("Selected item")
-st.write(timeline)
-
+with st.spinner(text="Building line"):
+    with open('timeline.json', "r") as f:
+        data = f.read()
+        timeline(data, height=500)
