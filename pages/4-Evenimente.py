b08c1a6a601fb82867c068658ca40fe368ad0887
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 23:43:58 +0200

    Update 4-Evenimente.py

--- pages/4-Evenimente.py
+++ pages/4-Evenimente.py
@@ -1,24 +1,35 @@
 from streamlit_elements import elements, mui, html
 import streamlit as st
 
-# Create a frame where Elements widgets will be displayed.
-#
-# Elements widgets will not render outside of this frame.
-# Native Streamlit widgets will not render inside this frame.
-#
-# elements() takes a key as parameter.
-# This key can't be reused by another frame or Streamlit widget.
+with elements("multiple_children"):
 
-with elements("new_element"):
-
-    # Let's create a Typography element with "Hello world" as children.
-    # The first step is to check Typography's documentation on MUI:
-    # https://mui.com/components/typography/
+    # You have access to Material UI icons using: mui.icon.IconNameHere
     #
-    # Here is how you would write it in React JSX:
+    # Multiple children can be added in a single element.
     #
-    # <Typography>
+    # <Button>
+    #   <EmojiPeople />
+    #   <DoubleArrow />
     #   Hello world
-    # </Typography>
+    # </Button>
 
-    mui.Typography("Hello world")
+    mui.Button(
+        mui.icon.EmojiPeople,
+        mui.icon.DoubleArrow,
+        "Button with multiple children"
+    )
+
+    # You can also add children to an element using a 'with' statement.
+    #
+    # <Button>
+    #   <EmojiPeople />
+    #   <DoubleArrow />
+    #   <Typography>
+    #     Hello world
+    #   </Typography>
+    # </Button>
+
+    with mui.Button:
+        mui.icon.EmojiPeople()
+        mui.icon.DoubleArrow()
+        mui.Typography("Button with multiple children")
