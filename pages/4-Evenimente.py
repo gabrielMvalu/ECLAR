613d48efd40530afe2490dd10ff10a0516489bc8
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 23:35:45 +0200

    Update 4-Evenimente.py

--- pages/4-Evenimente.py
+++ pages/4-Evenimente.py
@@ -1,1 +1,23 @@
+rom streamlit_elements import elements, mui, html
 
+# Create a frame where Elements widgets will be displayed.
+#
+# Elements widgets will not render outside of this frame.
+# Native Streamlit widgets will not render inside this frame.
+#
+# elements() takes a key as parameter.
+# This key can't be reused by another frame or Streamlit widget.
+
+with elements("new_element"):
+
+    # Let's create a Typography element with "Hello world" as children.
+    # The first step is to check Typography's documentation on MUI:
+    # https://mui.com/components/typography/
+    #
+    # Here is how you would write it in React JSX:
+    #
+    # <Typography>
+    #   Hello world
+    # </Typography>
+
+    mui.Typography("Hello world")
