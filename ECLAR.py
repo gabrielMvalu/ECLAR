127d247252454d29dbd6943308aa1af309b37cb0
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-17 19:49:57 +0200

    Update ECLAR.py

--- ECLAR.py
+++ ECLAR.py
@@ -1,5 +1,12 @@
 import streamlit as st 
 from streamlit_timeline import timeline
+from pyecharts import options as opts
+from pyecharts.charts import Pie
+from pyecharts.faker import Faker
+from streamlit_echarts import st_pyecharts
+
+
+
 
 st.set_page_config(layout="wide")
 st.header(':rainbow[Eclar STORY]')
@@ -10,3 +17,17 @@ with st.spinner(text="Building line"):
     with open('timeline.json', "r") as f:
         data = f.read()
         timeline(data, height=500)
+
+
+
+
+def render_basic_pie_chart():
+    c = (
+        Pie()
+        .add("", [list(z) for z in zip(Faker.choose(), Faker.values())])
+        .set_global_opts(title_opts=opts.TitleOpts(title="Exemplu de diagramă Pie"))
+        .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
+    )
+    st_pyecharts(c)
+
+render_basic_pie_chart()
