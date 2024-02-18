faeac855f266bcf40f6ce847379b3382026fa56d
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-18 21:22:56 +0200

    Update 3-Logistica.py

--- pages/3-Logistica.py
+++ pages/3-Logistica.py
@@ -1,4 +1,23 @@
 import streamlit as st 
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+    "col1": np.random.randn(1000) / 50 + 37.76,
+    "col2": np.random.randn(1000) / 50 + -122.4,
+    "col3": np.random.randn(1000) * 100,
+    "col4": np.random.rand(1000, 4).tolist(),
+})
+
+st.map(df,
+    latitude='col1',
+    longitude='col2',
+    size='col3',
+    color='col4')
+
+
+
 
 st.set_page_config(layout="wide")
 st.image("./data/logoECLAR.png", width=100) 
