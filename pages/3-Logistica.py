284073f6cbf40475327509e4de3cf20e0c211d02
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-18 22:45:56 +0200

    Update 3-Logistica.py

--- pages/3-Logistica.py
+++ pages/3-Logistica.py
@@ -19,7 +19,7 @@ organizației dumneavoastră.Aceste hărți oferă o perspectivă clară asupra 
 și livrărilor, elemente esențiale în planificarea și optimizarea campaniilor de marketing.
          """,
         sx={
-            "bgcolor": "background.blue",
+            "bgcolor": "blue",
             "boxShadow": 2,
             "borderRadius": 5,
             "p": 2,
