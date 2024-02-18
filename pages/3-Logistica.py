b282c0a015e26c42ef7d7e6bacbbdc934beae021
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-18 21:26:31 +0200

    Update 3-Logistica.py

--- pages/3-Logistica.py
+++ pages/3-Logistica.py
@@ -4,10 +4,10 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame({
-    "col1": np.random.randn(1000) / 50 + 23.35,
-    "col2": np.random.randn(1000) / 50 + 46.01,
-    "col3": np.random.randn(1000) * 100,
-    "col4": np.random.rand(1000, 4).tolist(),
+    "col1": 23.35,
+    "col2": 44.01,
+    "col3": np.random.randn(1000) * 200,
+    "col4": np.random.rand(1000, 10).tolist(),
 })
 
 st.map(df,
