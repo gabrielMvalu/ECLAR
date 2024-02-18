16786daa339758016ade513303c4667f1c0586b8
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-18 20:19:47 +0200

    Update 1-Home.py

--- pages/1-Home.py
+++ pages/1-Home.py
@@ -3,16 +3,7 @@ from streamlit_timeline import timeline
 
 st.set_page_config(layout="wide")
 
-# Utilizarea coloanelor pentru a plasa logo-ul lângă titlu
-col1, col2 = st.columns([1, 9])
-
-# În prima coloană, afișează logo-ul
-with col1:
-    st.image("./data/logoECLAR.png", width=100)  # Ajustează 'width' după necesități
-
-# În a doua coloană, afișează titlul
-with col2:
-    st.header(":blue[ECLAR SRL]")
+st.image("./data/logoECLAR.png", width=100)  # Ajustează 'width' după necesități
 
 st.divider()
 
