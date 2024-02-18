d6b1f271b7f2eefc25b76431084d0588227fb63e
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-18 21:28:13 +0200

    Update 3-Logistica.py

--- pages/3-Logistica.py
+++ pages/3-Logistica.py
@@ -4,15 +4,14 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame({
-    "col1": 23.35,
-    "col2": 44.01,
-    "col3": np.random.randn(1000) * 200,
-    "col4": np.random.rand(1000, 10).tolist(),
-})
+    "col1": np.random.randn(1000) / 50 + 44.01,
+    "col2": np.random.randn(1000) / 50 + 23.35,
+    "col3": np.random.randn(1000) * 100,
+    "col4": np.random.rand(1000, 4).tolist(),
 
 st.map(df,
-    latitude='col1',
-    longitude='col2',
+    latitude='44.01',
+    longitude='23.35',
     size='col3',
     color='col4')
 
